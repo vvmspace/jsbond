0568ab2ca449c69a749825f6ad3e11702e7f32dd
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-07-04 22:41:12 +0300

    +links

--- components/BondsFilter/BondsFilter.js
+++ components/BondsFilter/BondsFilter.js
@@ -20,7 +20,7 @@ class BondsFilter extends Component {
     render() {
         return <div className="block">
             <h3>Фильтр</h3>
-            <div><input name={'cheaper'} checked={this.props.filter.cheaper} type={'checkbox'} onChange={this.filterToggle}/> Дешевле номинала</div>
+            <div><input name={'cheaper'} checked={this.props.filter.cheaper} type={'checkbox'} onChange={this.filterToggle}/> <a title={'Облигации дешевле номинала'} href={'/cheaper'}>Дешевле номинала</a></div>
             <div><input name={'cheaper3'} checked={this.props.filter.cheaper3} type={'checkbox'} onChange={this.filterToggle}/> Дешевле 1.03 номинала</div>
             <div><input name={'monthly'} checked={this.props.filter.monthly} type={'checkbox'} onChange={this.filterToggle}/> Выплата в течении месяца</div>
             {/*<h3>Сортировка</h3>*/}
--- layout/NavBar/NavBar.js
+++ layout/NavBar/NavBar.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class NavBar extends Component {
     render() {
         return <div className={'navbar'}>
-            JS Bond
+            <a href={'/'}>JS Bond</a>
         </div>
     }
 }
