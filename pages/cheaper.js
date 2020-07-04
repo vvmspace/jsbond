806b7bbc7548a7529b28ffa8e28f4725d5a20d71
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-07-04 17:45:06 +0300

    +cheaper

---
+++ pages/cheaper.js
@@ -0,0 +1,61 @@
+import Layout from "../layout/Layout";
+import BondsList from "../components/BondsList/BondsList";
+import Head from "next/head";
+import axios from 'axios';
+
+const Home = props => {
+    return (
+        <Layout>
+            <div className="container">
+                <Head>
+                    <title>Облигации дешевле номинала</title>
+                    <link rel="icon" href="/favicon.ico"/>
+                </Head>
+                <BondsList bonds={props.bonds} />
+                {/*          <style jsx>{`*/}
+                {/*  .container {*/}
+                {/*    min-height: 100vh;*/}
+                {/*    padding: 0 0.5rem;*/}
+                {/*    display: flex;*/}
+                {/*    flex-direction: column;*/}
+                {/*    justify-content: center;*/}
+                {/*    align-items: center;*/}
+                {/*  }*/}
+
+                {/*`}</style>*/}
+            </div>
+        </Layout>
+    )
+}
+
+
+Home.getInitialProps = async function() {
+
+    console.log('GIP');
+
+    try {
+        const res = await axios.get('https://api.jsbond.ru/bonds');
+        const bonds = res.data
+            .filter(bond => bond.lastPrice < bond.faceValue)
+            // .filter(bond => bond.endDate < new Date().getTime() + 1 * 30 * 24 * 3600 * 1000)
+            // .filter(bond => bond.dateToClient < new Date().getTime() + 5 * 30 * 24 * 3600 * 1000)
+            // .sort((a,b) => a.yieldToClient < b.yieldToClient)
+            .sort((a,b) => 2 * (a.couponPeriodDays > b.couponPeriodDays ? 1 : a.couponPeriodDays < b.couponPeriodDays ? -1 : 0)
+                + (a.endDate > b.endDate ? 1 : a.endDate < b.endDate ? -1 : 0))
+            .filter(bond => (bond.dateToClient > new Date().getTime()))
+            .filter(bond => (bond.yieldToClient > 0))
+        ;
+
+        // console.log(bonds);
+
+
+        return {
+            bonds
+        };
+    } catch (e) {
+        return { errorCode: 502 };
+    }
+
+};
+
+export default Home;
