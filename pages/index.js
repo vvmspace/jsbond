6c8daf8fee8c6d043a39c88056640081e2c5d648
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-07-04 22:38:23 +0300

    front page

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,77 @@
 import Layout from "../layout/Layout";
 import BondsList from "../components/BondsList/BondsList";
+import BondsFilter from "../components/BondsFilter/BondsFilter";
 import Head from "next/head";
 import axios from 'axios';
+import React, { Component } from 'react';
 
-const Home = props => {
-    return (
-        <Layout>
-            <div className="container">
-                <Head>
-                    <title>JS Bond</title>
-                    <link rel="icon" href="/favicon.ico"/>
-                </Head>
-                {/*<div className="grid">*/}
-                    <a href="https://nextjs.org/docs" className="card">
-                        <h3>Documentation &rarr;</h3>
-                        <p>Find in-depth information about Next.js features and API.</p>
-                    </a>
-                {/*</div>*/}
-                <BondsList bonds={props.bonds} />
-      {/*          <style jsx>{`*/}
-      {/*  .container {*/}
-      {/*    min-height: 100vh;*/}
-      {/*    padding: 0 0.5rem;*/}
-      {/*    display: flex;*/}
-      {/*    flex-direction: column;*/}
-      {/*    justify-content: center;*/}
-      {/*    align-items: center;*/}
-      {/*  }*/}
+class Home extends Component {
 
-      {/*`}</style>*/}
-            </div>
-        </Layout>
-    )
+    state = {
+        filter: {
+            cheaper: false,
+            cheaper3: true,
+            monthly: true,
+        },
+    };
+
+    filterUpdate = filter => {
+        this.setState({filter});
+    }
+
+    filter = () => {
+        let bonds = this.props.bonds;
+        if (this.state.filter.cheaper) {
+            bonds = bonds.filter(bond => bond.lastPrice < bond.faceValue)
+        }
+
+        if (this.state.filter.cheaper3) {
+            bonds = bonds.filter(bond => bond.lastPrice < 1.03 * bond.faceValue)
+        }
+
+        if (this.state.filter.monthly) {
+            bonds = bonds.filter(bond => bond.endDate < new Date().getTime() + 31 * 24 * 3600 * 1000);
+        }
+
+        bonds
+            .sort((a,b) => a.yieldToClient < b.yieldToClient)
+            // .sort((a, b) => 2 * (a.couponPeriodDays > b.couponPeriodDays ? 1 : a.couponPeriodDays < b.couponPeriodDays ? -1 : 0)
+            // + (a.endDate > b.endDate ? 1 : a.endDate < b.endDate ? -1 : 0))
+            .filter(bond => (bond.dateToClient > new Date().getTime()))
+            .filter(bond => (bond.yieldToClient > 0));
+
+        return bonds;
+    }
+
+    render() {
+
+        const filtered = this.filter();
+
+        return (
+            <Layout>
+                <div className="container">
+                    <Head>
+                        <title>JS Bond</title>
+                        <link rel="icon" href="/favicon.ico"/>
+                    </Head>
+                    <h1>Облигации</h1>
+                    <BondsFilter onChange={this.filterUpdate} filter={this.state.filter} />
+                    <BondsList bonds={filtered}/>
+                    {/*          <style jsx>{`*/}
+                    {/*  .container {*/}
+                    {/*    min-height: 100vh;*/}
+                    {/*    padding: 0 0.5rem;*/}
+                    {/*    display: flex;*/}
+                    {/*    flex-direction: column;*/}
+                    {/*    justify-content: center;*/}
+                    {/*    align-items: center;*/}
+                    {/*  }*/}
+
+                    {/*`}</style>*/}
+                </div>
+            </Layout>
+        )
+    }
 }
 
 
