0828ef30d7a478b07e63c83f7e25ee89e2f33314
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-07-04 05:03:57 +0300

    +tinkoff

--- components/Tinkoff/Tinkoff.js
+++ components/Tinkoff/Tinkoff.js
@@ -1,5 +1,5 @@
 const Tinkoff = () => {
-    return <div className={'tinkoff'}>
+    return <div onClick={() => window.open('https://www.tinkoff.ru/sl/9nMXWP1hGXP')} className={'tinkoff'}>
         <div className={'img-wrapper'}><img src={'/tinkoff.svg'} /></div>
         <div className={'text-wrapper'}>Месяц бесплатной торговли и кардхолдер в подарок</div>
     </div>
